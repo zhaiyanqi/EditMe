3fa531b769036d58531fc4cd7ed79aa5ebd29df1
zhaiyanqi <zhaiyanqi929@gmail.com>
2019-04-01 13:44:44 +0800

    fix

--- app/src/main/java/cn/zhaiyanqi/editme/MainActivity.java
+++ app/src/main/java/cn/zhaiyanqi/editme/MainActivity.java
@@ -1,14 +1,34 @@
 package cn.zhaiyanqi.editme;
 
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private long exitTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/cn/zhaiyanqi/editme/SplashActivity.java
+++ app/src/main/java/cn/zhaiyanqi/editme/SplashActivity.java
@@ -1,14 +1,42 @@
 package cn.zhaiyanqi.editme;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private CardView cvLogo, cvLogo2;
+    private TextView tvEdit, tvMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        cvLogo = findViewById(R.id.cv_logo);
+        cvLogo = findViewById(R.id.cv_logo2);
+        tvEdit = findViewById(R.id.tv_edit);
+        tvMe = findViewById(R.id.tv_me);
+        new Handler().postDelayed(this::startTransition, 500);
+    }
+
+    private void startTransition() {
+        Intent intent = new Intent(this, MainActivity.class);
+        Pair pair = new Pair<>(cvLogo, "cv_logo");
+        Pair pair1 = new Pair<>(cvLogo, "cv_logo2");
+        Pair pair2 = new Pair<>(tvEdit, "tv_edit");
+        Pair pair3 = new Pair<>(tvMe, "tv_me");
+        ActivityOptionsCompat options =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        this, pair, pair1, pair2, pair3);
+        ActivityCompat.startActivity(this, intent, options.toBundle());
+        new Handler().postDelayed(() -> ActivityCompat.finishAfterTransition(this), 1000);
     }
 }
