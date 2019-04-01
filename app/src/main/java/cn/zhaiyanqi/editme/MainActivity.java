87b06462c2c76f32cf46de7fc5418a4b1cef6b25
zhaiyanqi <zhaiyanqi929@gmail.com>
2019-04-01 16:37:48 +0800

    transition

--- app/src/main/java/cn/zhaiyanqi/editme/MainActivity.java
+++ app/src/main/java/cn/zhaiyanqi/editme/MainActivity.java
@@ -1,20 +1,44 @@
 package cn.zhaiyanqi.editme;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     private long exitTime;
 
+    @BindView(R.id.root_layout)
+    ConstraintLayout rootLayout;
+    @BindView(R.id.et_prefix)
+    EditText etPrefix;
+    @BindView(R.id.et_suffix)
+    EditText etSuffix;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        new Handler().postDelayed(() -> {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }, 100);
     }
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -30,5 +54,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent me) {
+        if (me.getAction() == MotionEvent.ACTION_DOWN) {  //把操作放在用户点击的时候
+            View v = getCurrentFocus();      //得到当前页面的焦点,ps:有输入框的页面焦点一般会被输入框占据
+            if (isShouldHideKeyboard(v, me)) { //判断用户点击的是否是输入框以外的区域
+                hideKeyboard(v.getWindowToken());   //收起键盘
+                v.clearFocus();
+            }
+        }
+        return super.dispatchTouchEvent(me);
+    }
+
+    private boolean isShouldHideKeyboard(View v, MotionEvent event) {
+        if (v instanceof EditText) {
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0],
+                    top = l[1],
+                    bottom = top + v.getHeight(),
+                    right = left + v.getWidth();
+            return !(event.getX() > left) || !(event.getX() < right)
+                    || !(event.getY() > top) || !(event.getY() < bottom);
+        }
+        return false;
+    }
+
+    private void hideKeyboard(IBinder token) {
+        if (token != null) {
+            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
